422e4ea60750a21fec9b50dfc98aa47f6a80c0a5
Toxin23 <63064322+Toxin23@users.noreply.github.com>
2025-09-18 10:55:44 +0200

    Update Mailer.php

--- src/Mailer.php
+++ src/Mailer.php
@@ -1,11 +1,10 @@
 <?php
+namespace App;
 
 use PHPMailer\PHPMailer\PHPMailer;
 
-class Mailer
-{
-    public static function sendTicket($toEmail, $toName, $ticketCode, $qrPath)
-    {
+class Mailer {
+    public static function sendTicket($toEmail, $toName, $ticketCode, $qrPath) {
         $mail = new PHPMailer(true);
         $mail->isSMTP();
         $mail->Host = $_ENV['MAIL_HOST'];
@@ -14,13 +13,11 @@ class Mailer
         $mail->Password = $_ENV['MAIL_PASS'];
         $mail->SMTPSecure = 'tls';
         $mail->Port = $_ENV['MAIL_PORT'];
-
         $mail->setFrom($_ENV['MAIL_FROM'], $_ENV['MAIL_NAME']);
         $mail->addAddress($toEmail, $toName);
         $mail->Subject = 'Your Event Ticket';
         $mail->Body = "Hi $toName,\n\nHere is your ticket code: $ticketCode";
         $mail->addAttachment($qrPath);
-
         $mail->send();
     }
 }
